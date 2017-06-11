b033ecdd6ba2bcbfcd0b4d7e31fcc779426b455b
WeiChieh <urink8854@gmail.com>
2017-06-11 15:23:31 +0800

    更改上方Bar的內容
    1. 修正： audit.php th的字型大小
    2. 修正： navbar.php 上方欄位的內容，符合系統內容
    3. 新增： Applicaiton.php 增加一個function取得未審核的資料數量
    4. 新增： Admin.php 在需要取得數量的地方增加function並寫進session

--- application/views/layouts/navbar.php
+++ application/views/layouts/navbar.php
@@ -46,25 +46,21 @@
                 <?php
                 if ($this->session->has_userdata('name')):
                     echo "<ul class='nav navbar-nav navbar-left'>" .
-                        "<li>".
-                        "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>".
-                        "<i class='fa fa-dashboard'></i>".
-                        "</a>".
-                        "</li>".
+//                        "<li>".
+//                        "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>".
+//                        "<i class='fa fa-dashboard'></i>".
+//                        "</a>".
+//                        "</li>".
                         "<li class='dropdown'>".
                         "<a href='#' class='dropdown-toggle' data-toggle='dropdown'>".
-                        "<i class='fa fa-globe'></i>".
-                        "<b class='caret'></b><span class='notification'>5</span>".
+                        "<i class='fa fa-envelope-o'></i>".
+                        "<b class='caret'></b><span class='notification'>{$this->session->noaudit}</span>".
                         "</a>".
                         "<ul class='dropdown-menu'>".
-                        "<li><a href='#'>Notification 1</a></li>".
-                        "<li><a href='#'>Notification 2</a></li>".
-                        "<li><a href='#'>Notification 3</a></li>".
-                        "<li><a href='#'>Notification 4</a></li>".
-                        "<li><a href='#'>Another notification</a></li>".
+                        "<li><a href=".base_url('Admin/Audit')." onclick=localStorage.setItem('nav_active','2')>前往審核列表頁面</a></li>".
                         "</ul>".
                         "</li>".
-                        "<li><a href=''><i class='fa fa-search'></i></a></li>".
+//                        "<li><a href=''><i class='fa fa-search'></i></a></li>".
                         "</ul>";
                 endif;
                 ?>
